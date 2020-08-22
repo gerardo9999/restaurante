d040f7035215a1470802e2ee06ed187c5e218edd
gerardo9999 <56141101+gerardo9999@users.noreply.github.com>
2020-08-22 01:08:49 -0400

    7.1 xD

--- restaurante/resources/views/components/sidebar.blade.php
+++ restaurante/resources/views/components/sidebar.blade.php
@@ -1,128 +1,6 @@
 <div class="sidebar">
   <nav class="sidebar-nav">
       <ul class="nav">
-<<<<<<< HEAD
-        <li class="nav-item">
-          <a class="nav-link" href="/">
-            <i class="nav-icon icon-speedometer"></i> Dashboard
-            <span class="badge badge-primary"></span>
-          </a>
-        </li>
-        <li class="nav-title">Menu</li>
-
-        <li class="nav-item">
-            <a class="nav-link" href="/typography">
-              <i class="nav-icon icon-pencil"></i>Recepcion</a>
-        </li>
-
-        <li class="nav-item">
-          <a class="nav-link" href="/typography">
-            <i class="nav-icon icon-pencil"></i>Reserva</a>
-        </li>
-
-        <li class="nav-item">
-            <a class="nav-link" href="" target="_top">
-              <i class="nav-icon icon-drop"></i> Cliente</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="" target="_top">
-              <i class="nav-icon icon-drop"></i> Empleados</a>
-        </li>
-
-
-        <li class="nav-title">Configuracion</li>
-
-        <li class="nav-item nav-dropdown">
-            <a class="nav-link nav-dropdown-toggle" href="#">
-            <i class="nav-icon icon-puzzle"></i> Servicios</a>
-            <ul class="nav-dropdown-items">
-                <li class="nav-item">
-                    <a class="nav-link" href="" target="_top">
-                    <i class="nav-icon icon-puzzle"></i> Tipos</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="" target="_top">
-                    <i class="nav-icon icon-puzzle"></i> Categorias</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="" target="_top">
-                    <i class="nav-icon icon-puzzle"></i> Subcategorias</a>
-                </li>
-            </ul>
-        </li>
-
-
-        <li class="nav-item nav-dropdown">
-            <a class="nav-link nav-dropdown-toggle" href="#">
-            <i class="nav-icon icon-puzzle"></i> Habitaciones</a>
-            <ul class="nav-dropdown-items">
-                <li class="nav-item">
-                    <a class="nav-link" href="" target="_top">
-                    <i class="nav-icon icon-puzzle"></i> Habitaciones</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="" target="_top">
-                    <i class="nav-icon icon-puzzle"></i> Tipos</a>
-                </li>
-            </ul>
-        </li>
-
-        
-        <li class="nav-title">Informacion</li> 
-
-        <li class="nav-item nav-dropdown">
-            <a class="nav-link nav-dropdown-toggle" href="#">
-            <i class="nav-icon icon-cursor"></i> Reporte</a>
-            <ul class="nav-dropdown-items">
-                <li class="nav-item">
-                    <a class="nav-link" href="/buttons/buttons">
-                    <i class="nav-icon icon-cursor"></i> Reporte por dia</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/buttons/button-group">
-                    <i class="nav-icon icon-cursor"></i> Reporte por mes</a>
-                </li>
-            </ul>
-        </li>
-
-        
-
-        <li class="nav-item nav-dropdown">
-            <a class="nav-link nav-dropdown-toggle" href="#">
-            <i class="nav-icon icon-cursor"></i> Caja</a>
-            <ul class="nav-dropdown-items">
-                <li class="nav-item">
-                    <a class="nav-link" href="/buttons/buttons">
-                    <i class="nav-icon icon-cursor"></i> Apertura </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/buttons/button-group">
-                    <i class="nav-icon icon-cursor"></i>Cierre</a>
-                </li>
-            </ul>
-        </li>
-
-        <li class="nav-item">
-          <a class="nav-link" href="/widgets">
-            <i class="nav-icon icon-calculator"></i> Widgets
-            <span class="badge badge-primary">NEW</span>
-          </a>
-        </li>
-        
-        <li class="divider"></li>
-        <li class="nav-title">Sistema</li>
-
-        <li class="nav-item mt-auto">
-          <a class="nav-link nav-link-success" href="" target="_top">
-            <i class="nav-icon icon-cloud-download"></i> Bitacora</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link nav-link-danger" href="https://coreui.io/pro/" target="_top">
-            <i class="nav-icon icon-layers"></i> Acceso
-            <strong></strong>
-          </a>
-        </li>
-=======
           <li class="nav-item">
               <a class="nav-link active" href="main.html"><i class="icon-speedometer"></i> Escritorio</a>
           </li>
@@ -190,7 +68,6 @@
           <li class="nav-item">
               <a class="nav-link" href="main.html"><i class="icon-info"></i> Acerca de...<span class="badge badge-info">IT</span></a>
           </li>
->>>>>>> bcc1bf0e93e65ddeaf3fb9e3ac5fe6a0e7072ac2
       </ul>
   </nav>
   <button class="sidebar-minimizer brand-minimizer" type="button"></button>
