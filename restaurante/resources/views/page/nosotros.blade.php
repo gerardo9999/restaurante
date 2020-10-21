3319a75d90673fbde2381b63602b28dd60d732fd
GuadalupeQuina <guadalupe.quina@gmail.con>
2020-10-21 15:48:42 -0400

    cambios

--- restaurante/resources/views/page/nosotros.blade.php
+++ restaurante/resources/views/page/nosotros.blade.php
@@ -15,7 +15,7 @@
             <div class="row">
               <div class="col-sm-12 col-md-6">
                 <div class="testimonial-img">
-                  <img src="{{ asset('page/restaurante/img/testimonial-1.jpg') }}" alt="" class="img-fluid">
+                  <img src="{{ asset('page/restaurante/img/personal.jpg') }}" alt="" class="img-fluid">
                 </div>
               </div>
               <div class="col-sm-12 col-md-6">
@@ -24,14 +24,15 @@
                 </div>
                 <div class="testimonials-content">
                   <p class="testimonial-text">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, cupiditate ea nam praesentium
-                    debitis hic ber quibusdam
-                    voluptatibus officia expedita corpori.
+                  Lo más importante para nosotros es su satisfacción, 
+                  por eso nos dedicamos a brindarle una gran variedad de platos 
+                  y muchas opciones de sabores para satisfacer los gustos más exigentes. 
+                  Todo ello con material de primera calidad y elaboración especializada.
                   </p>
                 </div>
                 <div class="testimonial-author-box">
-                  <img src="{{ asset('page/restaurante/img/mini-testimonial-1.jpg') }}" alt="" class="testimonial-avatar">
-                  <h5 class="testimonial-author">Albert & Erika</h5>
+                  <img src="{{ asset('page/restaurante/img/personal.jpg') }}" alt="" class="testimonial-avatar">
+                  <h5 class="testimonial-author">Restaurante Pollos y Cuadriles "Roca Chavez"</h5>
                 </div>
               </div>
             </div>
@@ -42,7 +43,7 @@
             <div class="row">
               <div class="col-sm-12 col-md-6">
                 <div class="testimonial-img">
-                  <img src="{{ asset('page/restaurante/img/testimonial-2.jpg') }}" alt="" class="img-fluid">
+                  <img src="{{ asset('page/restaurante/img/personal2.jpg') }}" alt="" class="img-fluid">
                 </div>
               </div>
               <div class="col-sm-12 col-md-6">
@@ -51,14 +52,13 @@
                 </div>
                 <div class="testimonials-content">
                   <p class="testimonial-text">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, cupiditate ea nam praesentium
-                    debitis hic ber quibusdam
-                    voluptatibus officia expedita corpori.
+                  Restaurante Pollos y Cuadriles "Roca Chavez",tiene como objetivo llegar a todos los gustos con una amplia y variada carta.
+                  Disponemos de un amplio, acogedor y atractivo local.
                   </p>
                 </div>
                 <div class="testimonial-author-box">
-                  <img src="{{ asset('page/restaurante/img/mini-testimonial-2.jpg') }}" alt="" class="testimonial-avatar">
-                  <h5 class="testimonial-author">Pablo & Emma</h5>
+                  <img src="{{ asset('page/restaurante/img/personal2.jpg') }}" alt="" class="testimonial-avatar">
+                  <h5 class="testimonial-author">Restaurante Pollos y Cuadriles "Roca Chavez"</h5>
                 </div>
               </div>
             </div>
