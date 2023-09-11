b9afe2aed8e6e6f4ce27c4e7e6d2b1184a1a1f2e
gerardoariasjustiniano060 <128717100+gerardoariasjustiniano060@users.noreply.github.com>
2023-09-11 19:53:12 -0400

    page login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,55 +1,131 @@
-@extends('layouts.app')
-@section('content')
-<div class="card card-outline card-primary">
-    <div class="card-header text-center">
-        <a href="../../index2.html" class="h1"><b>Admin</b>LTE</a>
-    </div>
-    <div class="card-body">
-        <p class="login-box-msg">Sign in to start your session</p>
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-            <div class="input-group mb-3">
-                <input type="email" class="form-control" placeholder="Email" @error('email') is-invalid @enderror"
-                    name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                <div class="input-group-append">
-                    <div class="input-group-text">
-                        <span class="fas fa-envelope"></span>
-                    </div>
-                </div>
-            </div>
-            <div class="input-group mb-3">
-                <input type="password" class="form-control" placeholder="Password" @error('password') is-invalid
-                    @enderror" name="password" required autocomplete="current-password">
-                <div class="input-group-append">
-                    <div class="input-group-text">
-                        <span class="fas fa-lock"></span>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-8">
-                    <div class="icheck-primary">
-                        <input id="remember" type="checkbox" name="remember" id="remember" {{ old('remember')
-                            ? 'checked' : '' }}>
-                        <label for="remember">
-                            Recuerdanme
-                        </label>
-                    </div>
-                </div>
-                <!-- /.col -->
-                <div class="col-4">
-                    <button type="submit" class="btn btn-primary btn-block">Iniciar</button>
-                </div>
-                <!-- /.col -->
-            </div>
-        </form>
-        <p class="mb-1">
-            {{-- <a href="{{ route('password.request') }}">I forgot my password</a> --}}
-        </p>
-        <p class="mb-0">
-            {{-- <a href="register.html" class="text-center">Register a new membership</a> --}}
-        </p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3 | Log in (v2)</title>
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('admin-lte/plugins/fontawesome-free/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin-lte/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin-lte/dist/css/adminlte.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin-lte/plugins/toastr/toastr.min.css')}}">
+</head>
 
+<body class="hold-transition login-page">
+    <div class="login-box">
+        <div class="card card-outline card-primary">
+            <div class="card-header text-center">
+                <a href="#" class="h1"><b>Admin</b>LTE</a>
+            </div>
+            <div class="card-body">
+                <p class="login-box-msg">Inicie sessión</p>
+
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <div class="input-group mb-3">
+                        <input 
+                            id="email" 
+                            type="email" 
+                            class="form-control" 
+                            name="email" 
+                            value="{{ old('email') }}" 
+                            required 
+                            class="form-control" 
+                            placeholder="Email" 
+                            autocomplete="email" 
+                            autofocus
+                        >
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-envelope"></span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="input-group mb-3">
+                        <input  
+                            id="password" 
+                            type="password" 
+                            class="form-control" 
+                            name="password" 
+                            required 
+                            autocomplete="current-password"
+                        >
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-8">
+                            <div class="icheck-primary">
+                                <input type="checkbox" id="remember">
+                                <label for="remember">
+                                    Recuérdame
+                                </label>
+                            </div>
+                        </div>
+                        <!-- /.col -->
+                        <div class="col-4">
+                            <button type="submit" class="btn btn-primary btn-block">Iniciar</button>
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                </form>
+
+
+                 @if($errors->any())
+                    <script>
+                        window.addEventListener("load", function() {
+                            alertErrors()
+                        });
+                    </script>
+                @endif
+
+                <div class="social-auth-links text-center mt-2 mb-3">
+                    <a href="#" class="btn btn-block btn-primary">
+                        <i class="fab fa-facebook mr-2"></i> Iniciar sesión con Facebook
+                    </a>
+                    <a href="#" class="btn btn-block btn-danger">
+                        <i class="fab fa-google-plus mr-2"></i> Iniciar sesión con Google+
+                    </a>
+                </div>
+
+                <!-- /.social-auth-links -->
+                <p class="mb-1">
+                    <a href="forgot-password.html">He olvidado mi contraseña</a>
+                </p>
+            </div>
+            <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
     </div>
-</div>
-@endsection
+    <!-- /.login-box -->
+
+
+    <!-- jQuery -->
+    <script src="{{ asset('admin-lte/plugins/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('admin-lte/dist/js/adminlte.min.js') }}"></script>
+    <script src="{{ asset('admin-lte/plugins/toastr/toastr.min.js') }}"></script>
+
+    <script>
+        function alertErrors(){
+             $(document).Toasts('create', {
+                class: 'bg-danger',
+                title: 'Credenciales incorreptas',
+                body: 'Vuelve a ingresar tus credenciales por favor!',
+            });
+
+            setTimeout(function() {
+                $('.toast').toast('hide');
+            }, 3000);
+        }
+    </script>
+</body>
+</html>
