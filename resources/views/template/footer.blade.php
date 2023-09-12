342da1442033498926d83152e783e4929e4018cd
gerardoariasjustiniano060 <128717100+gerardoariasjustiniano060@users.noreply.github.com>
2023-09-11 20:06:33 -0400

    delete blade

--- resources/views/template/footer.blade.php
+++
@@ -1,8 +0,0 @@
-  <!-- /.content-wrapper -->
-  <footer class="main-footer">
-    <strong>Copyright &copy; 2023-20XX <a href="https://adminlte.io">Gerard Arias</a>.</strong>
-    Todos los derechos reservados.
-    <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.2.0
-    </div>
-  </footer>
