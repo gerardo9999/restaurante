3e4572ec14a64d7525632ca3af7cd74d55c180c7
gerardoariasjustiniano060 <128717100+gerardoariasjustiniano060@users.noreply.github.com>
2023-09-12 19:31:52 -0400

    style users

---
+++ resources/views/template/dist/style-datatable.blade.php
@@ -0,0 +1,28 @@
+<link rel="stylesheet" href="{{ asset('admin-lte/plugins/fontawesome-free/css/all.min.css') }}">
+<!-- DataTables -->
+<link rel="stylesheet" href="{{ asset('admin-lte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+<link rel="stylesheet"
+    href="{{ asset('admin-lte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+<link rel="stylesheet" href="{{ asset('admin-lte/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
+<!-- Theme style -->
+<link rel="stylesheet" href="{{ asset('admin-lte/dist/css/adminlte.min.css') }}">
+  <!-- Ionicons -->
+<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+
+<style>
+    /*estilos para la tabla*/
+    table th {
+        background-color: #689fcf !important;
+        color: white;
+    }
+
+    table>tbody>tr>td {
+        vertical-align: middle !important;
+    }
+
+    /*para alinear los botones y cuadro de busqueda*/
+    .btn-group,
+    .btn-group-vertical {
+        position: absolute !important;
+    }
+</style>
