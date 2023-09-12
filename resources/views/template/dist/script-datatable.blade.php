802171c3c1f8ff6011fd1d2f1e09fdffd19c2a6b
gerardoariasjustiniano060 <128717100+gerardoariasjustiniano060@users.noreply.github.com>
2023-09-12 19:38:49 -0400

    script datatable

---
+++ resources/views/template/dist/script-datatable.blade.php
@@ -0,0 +1,21 @@
+    <!-- jQuery -->
+    <script src="{{ asset('admin-lte/plugins/jquery/jquery.min.js') }}"></script>
+    <!-- Bootstrap 4 -->
+    <script src="{{ asset('admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <!-- DataTables  & Plugins -->
+    <script src="{{ asset('admin-lte/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('admin-lte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('admin-lte/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('admin-lte/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('admin-lte/plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('admin-lte/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('admin-lte/plugins/jszip/jszip.min.js') }}"></script>
+    <script src="{{ asset('admin-lte/plugins/pdfmake/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('admin-lte/plugins/pdfmake/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('admin-lte/plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('admin-lte/plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+    <script src="{{ asset('admin-lte/plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
+    <!-- AdminLTE App -->
+    <script src="{{ asset('admin-lte/dist/js/adminlte.min.js') }}"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="{{ asset('admin-lte/dist/js/demo.js') }}"></script>
