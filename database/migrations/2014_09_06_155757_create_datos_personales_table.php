a1e0abea06385e3b1bbb8f66781a3a20400d4feb
gerardoariasjustiniano060 <128717100+gerardoariasjustiniano060@users.noreply.github.com>
2023-09-11 20:03:55 -0400

    migration datos personales

---
+++ database/migrations/2014_09_06_155757_create_datos_personales_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('datos_personales', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombres')->nullable();
+            $table->string('apellidos')->nullable();
+            $table->string('ci')->nullable();
+            $table->string('telefono')->nullable();
+            $table->string('direccion')->nullable();
+            $table->date('fecha_nacimiento')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('datos_personales');
+    }
+};
