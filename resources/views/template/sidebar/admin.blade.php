2edcf233e64be3647fa4168012ab09a3ef37e903
gerardoariasjustiniano060 <128717100+gerardoariasjustiniano060@users.noreply.github.com>
2023-09-12 19:38:16 -0400

    sidebar option users

--- resources/views/template/sidebar/admin.blade.php
+++ resources/views/template/sidebar/admin.blade.php
@@ -290,6 +290,12 @@
                   <p>Permisos</p>
                 </a>
               </li>
+              <li class="nav-item">
+                <a href="{{ route('users') }}" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Usuarios</p>
+                </a>
+              </li>
             </ul>
           </li>
           <li class="nav-item">
