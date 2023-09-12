214aa95fe67a6f57a2fee8956e484a76fc5f1a30
gerardoariasjustiniano060 <128717100+gerardoariasjustiniano060@users.noreply.github.com>
2023-09-11 20:04:16 -0400

    migration users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,26 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('avatar')->default('images/avatar.svg');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+
+            $table->foreignId('rol_id')->references('id')->on('roles')->onDelete('cascade');
+
+            $table->foreignId('empresa_id')
+                ->nullable()
+                ->references('id')
+                ->on('empresas')
+                ->onDelete('cascade');
+
+            $table->foreignId('dato_personal_id')
+                    ->nullable()
+                   ->references('id')
+                   ->on('datos_personales')
+                   ->onDelete('cascade');
+
             $table->timestamps();
         });
     }
