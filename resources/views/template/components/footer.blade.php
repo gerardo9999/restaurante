6de7b773803cd9be3d2ce31317e501a85a065bd8
gerardoariasjustiniano060 <128717100+gerardoariasjustiniano060@users.noreply.github.com>
2023-09-11 20:02:25 -0400

    footer template

---
+++ resources/views/template/components/footer.blade.php
@@ -0,0 +1,8 @@
+  <!-- /.content-wrapper -->
+  <footer class="main-footer text-sm">
+    <strong>Copyright &copy; 2023-20XX <a href="https://adminlte.io">Gerard Arias</a>.</strong>
+    Todos los derechos reservados.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 1.0.0
+    </div>
+  </footer>
