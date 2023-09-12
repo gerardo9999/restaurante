80006c1efa5ccfea4a34fea81447a57e1bdf7ccd
gerardoariasjustiniano060 <128717100+gerardoariasjustiniano060@users.noreply.github.com>
2023-09-11 20:01:51 -0400

    layout configuration

---
+++ resources/views/layouts/app-configuracion.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Dashboard</title>
+  @include('template.dist.style-dashboard')
+</head>
+{{-- sidebar-collapse --}}
+{{-- layout-footer-fixed --}}
+<body class="hold-transition sidebar-mini layout-fixed sidebar-collapse layout-navbar-fixed">
+<div class="wrapper">
+
+    <!-- Preloader -->
+    <div class="preloader flex-column justify-content-center align-items-center">
+        <img class="animation__shake" src="{{ asset('admin-lte/dist/img/AdminLTELogo.png')}}" alt="AdminLTELogo" height="60" width="60">
+    </div>
+
+    @include('template.navbar.admin')
+    @include('template.sidebar.admin')
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+       @yield('content')
+    </div>
+    <!-- /.content-wrapper -->
+    @include('template.components.footer')
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark"></aside>
+</div>
+    @include('template.dist.script-dashboard')
+</body>
+</html>
